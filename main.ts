a7ca564ff26a9f1a737e4f1b09a45569afe50634
Kyphong Pham <130786963+Kyphong-Pham@users.noreply.github.com>
2025-09-19 13:21:54 -0400

    Now shows temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyphong Pham
+ * Created on: Sep 2025
+ * This program shows the temperature when A button pressed
 */
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
 
-basic.showString('Hello, World!')
+// When A button pressed temperature is shown
+let temperature: number
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString('The Temperature Is:' + input.temperature() )
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    
+})
+
